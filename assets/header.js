160f7ffb860d79d87c862b110e4945acb35ebe3f
Fourth Wink <winkfourth@gmail.com>
2026-06-01 17:36:33 +0300

    Revert laptop header breakpoint changes

--- assets/header.js
+++ assets/header.js
@@ -113,18 +113,7 @@ class HeaderComponent extends Component {
    * @param {OverflowMinimumEvent} event
    */
   #handleOverflowMinimum = (event) => {
-    if (window.innerWidth >= 1200) {
-      this.#updateMenuVisibility(false);
-      return;
-    }
-
     this.#updateMenuVisibility(event.detail.minimumReached);
-  };
-
-  #handleWindowResize = () => {
-    if (window.innerWidth >= 1200) {
-      this.#updateMenuVisibility(false);
-    }
   };
 
   /**
@@ -213,7 +202,6 @@ class HeaderComponent extends Component {
     super.connectedCallback();
     this.#resizeObserver.observe(this);
     this.addEventListener('overflowMinimum', this.#handleOverflowMinimum);
-    window.addEventListener('resize', this.#handleWindowResize);
 
     const stickyMode = this.getAttribute('sticky');
     if (stickyMode) {
@@ -230,7 +218,6 @@ class HeaderComponent extends Component {
     this.#resizeObserver.disconnect();
     this.#intersectionObserver?.disconnect();
     this.removeEventListener('overflowMinimum', this.#handleOverflowMinimum);
-    window.removeEventListener('resize', this.#handleWindowResize);
     document.removeEventListener('scroll', this.#handleWindowScroll);
     if (this.#scrollRafId !== null) {
       cancelAnimationFrame(this.#scrollRafId);
